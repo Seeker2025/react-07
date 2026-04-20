dde47ab21d7191ac806aaa4fad707f027aec40c8
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-04-20 13:01:55 +0300

    go react

--- src/components/Forma/Forma.jsx
+++ src/components/Forma/Forma.jsx
@@ -4,23 +4,27 @@ export class Forma extends Component{
     state = {
         name: '',
         tag: '',
+        experience: 'junior',
+        licence: false,
     };
 
     handleChange = e =>{
-        const { name, value } = e.currentTarget;
-
+        const { name, value  } = e.currentTarget;
         this.setState({ [name]: value });
-    }
+    };
 
     handleSubmit = e =>{
         e.preventDefault();
         console.log(this.state);
-        
-    }
+    };
+
+    handleLicenceChange = e => {
+        console.log(e.currentTarget.checked);
+        this.setState({licence: e.currentTarget.checked})
+    };
 
     render(){
         return(
-
 
     <form onSubmit ={this.handleSubmit}>
             <label>
@@ -43,13 +47,55 @@ export class Forma extends Component{
                 />
             </label>
 
-            <button type="submit">Button</button>
+            <p>Ваш уровень</p>
+            
+                <label>
+                    junior
+                    <input
+                     type="radio"
+                     name="experience"
+                     value="junior"
+                     onChange={this.handleChange}
+                     checked={this.state.experience === 'junior'}
+                     />
+                </label>
+
+                <label>
+                    middle
+                    <input
+                     type="radio"
+                     name="experience"
+                     value="middle"
+                     onChange={this.handleChange}
+                     checked={this.state.experience === 'middle'}
+                    />
+
+                </label>
+
+                <label>
+                    senior
+                    <input
+                     type="radio"
+                     name="experience"
+                     value="senior"
+                     onChange={this.handleChange}
+                     checked={this.state.experience === 'senior'}
+                     />
+                </label>
+
+                <label>
+                    <input
+                      type ="checkbox"
+                      name="licence"
+                      checked={this.state.licence}
+                      onChange={this.handleLicenceChange}
+                      />
+                      Согласен с условием
+                </label>    
+
+            <button type="submit" disabled={!this.state.licence}>Button</button>
     </form>
-
-
 
         );
     }
-
-
 }
